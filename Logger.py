9bf10e4a939de432ecd34ba3f19cef8252e5bf51
JulianH <you@example.com>
2022-06-16 21:39:19 +0200

    Debugging.

--- Logger.py
+++ Logger.py
@@ -5,6 +5,6 @@ import logging
 
 # Ustawienia logowania informacji
 logger = logging.getLogger('dist_monit')
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 FORMAT = '[ %(levelname)s\t%(asctime)s %(name)s ] %(message)s'
 logging.basicConfig(format=FORMAT, datefmt='%m/%d/%Y %I:%M:%S %p')
